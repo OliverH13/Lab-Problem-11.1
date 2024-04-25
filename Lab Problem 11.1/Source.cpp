2dfeb3c567b717ef899de4f7b271841acd0bee27
Oliver Hendricks <olliehendricks04@gmail.com>
2024-04-25 00:59:58 -0400

    finished display_seat_column

--- Lab Problem 11.1/Source.cpp
+++ Lab Problem 11.1/Source.cpp
@@ -1,45 +1,98 @@
 
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 const int ROWS = 13;
+const int COLUMNS = 8;
 
-int find_row_index(int this_row);
-int find_seat_index(char this_seat);
-void display_seat_column(char seats[][ROWS], int column);
+int findRowIndex(int thisRow);
+int findSeatIndex(char thisSeat);
+void displaySeatColumn(char seats[][ROWS], int COLUMNS);
 
-int main() {
-	char seats[50][ROWS];
-	int seat;
+int main(void) {
+
+	char seats[COLUMNS][ROWS];
+	char thisSeat;
+	int thisRow;
+	int row;
+	int column;
+
+	for (int column = 0; column < COLUMNS; column++) { 
+		char cnt = '1';
+		char cnt2 = '0';
+		for (int row = 0; row < ROWS; row++) {
+			if (column == 0 && row != 12) seats[0][row] = 'D';
+			if (column == 0 && row == 12) seats[0][row] = ' ';
+			if (column == 1 && row != 12) seats[1][row] = 'C';
+			if (column == 1 && row == 12) seats[1][row] = ' ';
+			if (column == 2) seats[2][row] = ' ';
+			if (column == 3 && row < 9) seats[3][row] = ' ';
+			if (column == 3 && row >= 9) seats[3][row] = '1';
+			if (column == 4 && row < 9) {
+				seats[4][row] = cnt;
+				cnt++;
+			}
+			if (column == 4 && row >= 9) {
+				seats[4][row] = cnt2;
+				cnt2++;
+			}
+			if (column == 5) seats[5][row] = ' ';
+			if (column == 6) seats[6][row] = 'B';
+			if (column == 7) seats[7][row] = 'A';
+		}
+	}
 
 	while (true) {
-		display_seat_column(seats, 4);
+
+		displaySeatColumn(seats, COLUMNS);
 		cout << "Enter a seat or Q to quit: ";
-		cin >> seat;
+		cin >> thisRow >> thisSeat;
+
 		if (cin.fail()) break;
 
+		row = findRowIndex(thisRow);
+		column = findSeatIndex(thisSeat);
+
+		if (row == ROWS + 1 || column == COLUMNS + 1) {
+			cout << "Sorry, no such seat exists on the CRJ 200. " << endl;
+		}
+		else if (row == 12 && column == 0) cout << "Sorry, no such seat exists on the CRJ 200. " << endl;
+		else if (row == 12 && column == 1) cout << "Sorry, no such seat exists on the CRJ 200. " << endl;
+
+		else if (seats[column][row] != 'X') {
+			seats[column][row] = 'X';
+		}
+		else if (seats[column][row] == 'X') {
+			cout << "This seat has already been taken" << endl;
+		}
 	}
 }
 
-int find_row_index(int this_row) {
-
-
-
-	return 0;
+int findRowIndex(int thisRow) {
+	if (thisRow > 0 && thisRow <= ROWS) return thisRow - 1;
+	else {
+		return ROWS + 1;
+	}
 }
 
-int find_seat_index(char this_seat) {
-
-
-
-	return 0;
+int findSeatIndex(char thisSeat) {
+	if (thisSeat == 'D') return 0;
+	else if (thisSeat == 'C') return 1;
+	else if (thisSeat == 'B') return 6;
+	else if (thisSeat == 'A') return 7;
+	else {
+		return COLUMNS + 1; 
+	} 
 }
 
-void display_seat_column(char seats[][ROWS], int column) {
-	cout << endl << "D D D D D D D D D D D D" << endl << "C C C C C C C C C C C C" << endl;
-	cout << endl << "                 " << " 1" << " 1" << " 1" << " 1" << endl;
-	cout << "1" << " 2" << " 3" << " 4" << " 5" << " 6" << " 7" << " 8" << " 9" << " 0" << " 1" << " 2" << " 3" << endl << endl;
-	cout << "B B B B B B B B B B B B B" << endl << "A A A A A A A A A A A A A" << endl << endl;
+void displaySeatColumn(char seats[][ROWS], int column) {
+	cout << endl;
+	for (int column = 0; column < COLUMNS; column++) {
+		for (int row = 0; row < ROWS; row++) {
+			cout << seats[column][row] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
